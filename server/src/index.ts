5e6011c22c73f62f6d5663bb074e598a154c5950
munirsoftdev <muniryahaya2002@gmail.com>
2026-04-28 06:09:28 -0700

    fix: update build script for production

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,7 +9,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+	cors({
+		origin: "https://munirSoftdev.onrender.com",
+	}),
+);
 app.use(express.json());
 
 // 1. Configure the Rate Limiter

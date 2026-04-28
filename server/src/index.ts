d4296f71ed8eef051483be8ec75ee28a09cebbf7
munirsoftdev <muniryahaya2002@gmail.com>
2026-04-28 15:34:40 -0700

    move set proxy to top

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,6 +9,7 @@ import "dotenv/config";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.set("trust proxy", 1);
 // Middleware
 app.use(helmet());
 app.use(
@@ -84,8 +85,6 @@ app.post(
 app.get("/health", (req, res) => {
 	res.status(200).send("Server is awake!");
 });
-
-app.set("trust proxy", 1);
 
 app.listen(PORT, () => {
 	console.log(`🚀 Server running on ${PORT}`);

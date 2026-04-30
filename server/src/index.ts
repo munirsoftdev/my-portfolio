5270eadbc72f8ff80576b91f2e847d7d5b4338db
munirsoftdev <muniryahaya2002@gmail.com>
2026-04-29 18:37:24 -0700

    changed rate limit to 100

--- server/src/index.ts
+++ server/src/index.ts
@@ -26,7 +26,7 @@ app.use(express.json());
 // 1. Configure the Rate Limiter
 const contactLimiter = rateLimit({
 	windowMs: 15 * 60 * 1000, // 15 minutes
-	max: 10, // Limit each IP to 10 requests per windowMs
+	max: 100, // Limit each IP to 100 requests per windowMs
 	message: {
 		success: false,
 		message:
